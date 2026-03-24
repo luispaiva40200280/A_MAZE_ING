05e35994116a2cf17aa3ef80b1da754afa77570d
luispaiva40200280 <luispaiva8936@gmail.com>
2026-03-24 01:06:11 +0000

    just a small test

--- Code/a_maze_ing.py
+++ Code/a_maze_ing.py
@@ -1,3 +1,9 @@
+from pydantic import BaseModel
+
+
+class Maze(BaseModel):
+    pass
+
 
 if __name__ == "__main__":
     print("ola isto é so um test...")
--- main.py
+++
@@ -1,2 +0,0 @@
-# this main is for testting
-
